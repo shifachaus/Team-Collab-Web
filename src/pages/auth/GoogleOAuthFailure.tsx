280d480c610f32071b99886d05e8966703f953f7
shifachaus <chausshifa804@gmail.com>
2025-08-28 18:40:16 +0530

    style: add Google OAuth failure page

--- src/pages/auth/GoogleOAuthFailure.tsx
+++ src/pages/auth/GoogleOAuthFailure.tsx
@@ -1,5 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Link, useNavigate } from "react-router-dom";
+
 const GoogleOAuthFailure = () => {
-  return <div>GoogleOAuthFailure</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          to="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          Team Collab.
+        </Link>
+        <div className="flex flex-col gap-6"></div>
+      </div>
+      <Card>
+        <CardContent>
+          <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Authentication Failed</h1>
+            <p>We couldn't sign you in with Google. Please try again.</p>
+          </div>
+          <Button onClick={() => navigate("/")} style={{ marginTop: "20px" }}>
+            Back to Login
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default GoogleOAuthFailure;
