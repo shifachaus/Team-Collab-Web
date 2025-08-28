4272dd32126459fe7d3c47cd5ad87a68f535bbdc
shifachaus <chausshifa804@gmail.com>
2025-08-28 18:37:13 +0530

    style: update sign-up/sign-in pages with Google OAuth button and failure page

--- src/pages/auth/Sign-in.tsx
+++ src/pages/auth/Sign-in.tsx
@@ -1,6 +1,32 @@
+import GoogleOauthButton from "@/components/auth/google-oauth-button";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Loader } from "lucide-react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
@@ -11,6 +37,101 @@ const SignIn = () => {
           Team Collab.
         </Link>
         <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader className="text-center">
+              <CardTitle className="text-xl">Welcome back</CardTitle>
+              <CardDescription>
+                Login with your Email or Google account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Form {...form}>
+                <form>
+                  <div className="grid gap-6">
+                    <div className="flex flex-col gap-4">
+                    <GoogleOauthButton label="Login" />
+                    </div>
+
+                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                      <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        Or continue with
+                      </span>
+                    </div>
+
+                    <div className="grid gap-3">
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="email"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                                Email
+                              </FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder="m@example.com"
+                                  className="!h-[48px]"
+                                  {...field}
+                                />
+                              </FormControl>
+
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="password"
+                          render={({ field }) => (
+                            <FormItem>
+                              <div className="flex items-center">
+                                <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                                  Password
+                                </FormLabel>
+
+                                <a
+                                  href="#"
+                                  className="ml-auto text-xs underline-offset-4 hover:underline"
+                                >
+                                  Forgot your password?
+                                </a>
+                              </div>
+                              <FormControl>
+                                <Input
+                                  type="password"
+                                  className="!h-[48px]"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+
+                      <Button disabled type="submit" className="w-full">
+                        <Loader className="animate-spin" />
+                      </Button>
+                    </div>
+                    <div className="text-center text-sm">
+                      Don&apos;t have an account?{" "}
+                      <Link
+                        to="/sign-up"
+                        className="underline underline-offset-4"
+                      >
+                        Sign Up
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+
           <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
             By clicking continue, you agree to our{" "}
             <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
--- src/pages/auth/Sign-up.tsx
+++ src/pages/auth/Sign-up.tsx
@@ -1,7 +1,170 @@
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Link } from "react-router-dom";
+import { Loader } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import GoogleOauthButton from "@/components/auth/google-oauth-button";
 
-export default SignUp
+const SignUp = () => {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          to="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          Team Collab.
+        </Link>
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader className="text-center">
+              <CardTitle className="text-xl">Create an account</CardTitle>
+              <CardDescription>
+                Signup with your Email or Google account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Form {...form}>
+                <form>
+                  <div className="grid gap-6">
+                    <div className="flex flex-col gap-4">
+                    <GoogleOauthButton label="Signup" />
+                    </div>
+
+                    <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                      <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                        Or continue with
+                      </span>
+                    </div>
+
+                    <div className="grid gap-3">
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="name"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                                Name
+                              </FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder="Joh Doe"
+                                  className="!h-[48px]"
+                                  {...field}
+                                />
+                              </FormControl>
+
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="email"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                                Email
+                              </FormLabel>
+                              <FormControl>
+                                <Input
+                                  placeholder="m@example.com"
+                                  className="!h-[48px]"
+                                  {...field}
+                                />
+                              </FormControl>
+
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+
+                      <div className="grid gap-2">
+                        <FormField
+                          control={form.control}
+                          name="password"
+                          render={({ field }) => (
+                            <FormItem>
+                              <div className="flex items-center">
+                                <FormLabel className="dark:text-[#f1f7feb5] text-sm">
+                                  Password
+                                </FormLabel>
+
+                                <a
+                                  href="#"
+                                  className="ml-auto text-xs underline-offset-4 hover:underline"
+                                >
+                                  Forgot your password?
+                                </a>
+                              </div>
+                              <FormControl>
+                                <Input
+                                  type="password"
+                                  className="!h-[48px]"
+                                  {...field}
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+
+                      <Button disabled type="submit" className="w-full">
+                        <Loader className="animate-spin" />
+                      </Button>
+                    </div>
+                    <div className="text-center text-sm">
+                      Don&apos;t have an account?{" "}
+                      <Link
+                        to="/"
+                        className="underline underline-offset-4"
+                      >
+                        Sign in
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+
+          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+            By clicking continue, you agree to our{" "}
+            <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
