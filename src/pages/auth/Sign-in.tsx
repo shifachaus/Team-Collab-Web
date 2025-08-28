86cdb90639935bce738d1ed781adaa236c76e013
shifachaus <chausshifa804@gmail.com>
2025-08-28 17:55:52 +0530

    chore: install Radix UI and shadcn/ui packages and update Tailwind configuration

--- src/pages/auth/Sign-in.tsx
+++ src/pages/auth/Sign-in.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          to="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          Team Collab.
+        </Link>
+        <div className="flex flex-col gap-6">
+          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+            By clicking continue, you agree to our{" "}
+            <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
